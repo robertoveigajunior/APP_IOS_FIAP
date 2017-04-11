4f845f4d8d1b8810137ded284452d29262483695
Roberto Veiga Junior <robertoveigajunior@MacBook-Air-de-Roberto.local>
2017-04-11 02:29:19 -0300

    save the product

--- RobertoRenan/RegisterViewController.swift
+++ RobertoRenan/RegisterViewController.swift
@@ -15,6 +15,9 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var tfPurchaseState: UITextField!
     @IBOutlet weak var tfPrice: UITextField!
     
+    var stateSelected: State?
+    var imageSelected: UIImage?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -28,6 +31,53 @@ class RegisterViewController: UIViewController {
     }
     
     @IBAction func saveProduct(_ sender: UIBarButtonItem) {
+        save()
+    }
+}
+
+//MARK: - Methods
+
+extension RegisterViewController {
+    
+    func save() {
+        let product = Product(context: context)
+        
+        if let name = tfNameProduct.text?.validator(context: self) {
+            product.name = name
+        }else if let image = imageSelected {
+            product.image = image
+        }else if let purchaseState = stateSelected {
+            product.states = purchaseState
+        }else if let price = tfPrice.text?.validator(context: self) {
+            product.price = Double(price)!
+        }
         
     }
 }
+
+extension String {
+    func validator(context: UIViewController) -> String? {
+        guard !self.isEmpty else {
+            let alert = UIAlertController(title: "Atenção", message: "Preencha todos os campos", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "Ok", style: .destructive, handler: nil)
+            alert.addAction(okAction)
+            context.present(alert, animated: true, completion: nil)
+            return nil
+        }
+        return self
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
